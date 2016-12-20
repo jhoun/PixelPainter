642e102b713dad7d51f954c039e3d87f6c01ad00
jhoun <jayhoun@gmail.com>
2016-12-19 20:57:32 -1000

    made it visually more visually appealing

--- public/js/PixelPainter.js
+++ public/js/PixelPainter.js
@@ -26,7 +26,7 @@ main.appendChild(gridsDiv);
   '#061773', '#371b75', '#0c1e63', '#113cf6', '#682076', '#6e46d9', '#6393e5',
   '#77faf9', '#3b8180', '#3a3c7e', '#51baf6', '#b0d3e0', '#55a9a0', '#91caf4',
   '#adbed8','#685b7f', '#686be5', '#3b6bd7', '#595bc2', '#697786', '#4c7ba9',
-  '#367575', '#74d7c8', '#a2a2a2', '#cecfce', '#000000'];
+  '#367575', '#74d7c8', '#a2a2a2', '#cecfce', '#000000', '#da8080', '#7bf194', '#79741b', '#487e4f',];
 
     function createColorDiv(numRows, numCell) {
 
@@ -40,15 +40,15 @@ main.appendChild(gridsDiv);
         cell.classList.add('colors');
         cell.style.border = "thin solid black"
         cell.style.display = "inline-block";
-        cell.style.height = '10px';
-        cell.style.width = '10px';
+        cell.style.height = '18px';
+        cell.style.width = '18px';
       }
     }
   }
 
 
 
-  createColorDiv(11,6);
+  createColorDiv(14,5);
 
 // creates the colorSwatch and lets you select a color to paint
   var colorCells =  document.querySelectorAll('div.colors');
@@ -76,8 +76,10 @@ var clicked = false;
         row.appendChild(cell);
 
         cell.style.backgroundColor = 'none';
+
         cell.classList.add('paintGridCells');
         cell.id = "div" + x;
+        cell.style.backgroundColor = 'none';
         cell.style.display = "inline-block";
         cell.style.height = '50px';
         cell.style.width = '50px';
