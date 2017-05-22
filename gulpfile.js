84487f2cb656f3aa5172cbc5b9d53d2585378786
jhoun <jayhoun@gmail.com>
2017-05-22 16:28:25 -0700

    updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var connect = require('gulp-connect');
 gulp.task('connect', function(){
   connect.server({
     root: 'public',
-    port: process.env.PORT
+    port: process.env.PORT,
     livereload: false
   });
 });
