190e773a94f75d414edc443aa78fdd4db7cef128
jhoun <jayhoun@gmail.com>
2017-05-22 16:42:27 -0700

    updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 
-// gulp.task('connect', function(){
-//   connect.server({
-//     root: 'public',
-//     port: process.env.PORT,
-//     livereload: false
-//   });
-// });
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    port: process.env.PORT,
+    livereload: false
+  });
+});
 
 // keeps gulp from crashing for scss errors
 gulp.task('sass', function () {
@@ -17,14 +17,14 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
-});
+// gulp.task('livereload', function (){
+//   gulp.src('./public/**/*')
+//   .pipe(connect.reload());
+// });
 
 gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
   gulp.watch('./public/**/*', ['livereload']);
 });
 
-gulp.task('default', [/*'connect', */'watch', 'sass']);
+gulp.task('default', ['connect', 'watch', 'sass']);
